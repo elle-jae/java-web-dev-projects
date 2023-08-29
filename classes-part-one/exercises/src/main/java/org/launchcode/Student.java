0d40ebc832085f1f434f4257481bb871d5b04377
e <ellen.jardon@gmail.com>
2023-08-28 21:06:19 -0500

    completed exercise

--- classes-part-one/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-one/exercises/src/main/java/org/launchcode/Student.java
@@ -1,12 +1,19 @@
+package org.launchcode;
+
 public class Student {
         private String name;
         private int studentId;
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
-        // Drop your getters and setters below for the Student class.
-        // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
-
+        // Drop your getters and setters below for the org.launchcode.Student class.
+        // To instantiate the org.launchcode.Student class, add your code to the main in the file, SchoolPractice.
+public Student(String name, int studentId, int numberOfCredits, double gpa) {
+    this.name = name;
+    this.studentId = studentId;
+    this.numberOfCredits = numberOfCredits;
+    this.gpa = gpa;
+}
 public String getName() {
         return name;
 }
--- classes-part-one/exercises/src/main/java/org/launchcode/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/org/launchcode/StudentPractice.java
@@ -1,6 +1,8 @@
+package org.launchcode;
+
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //insantiate your org.launchcode.Student class below
         Student student = new Student("Ellen", 1234, 1, 4.0);
     }
 }
